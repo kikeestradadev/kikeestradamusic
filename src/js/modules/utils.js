8ddf650cdcec7db134a90fde2b961faf9cda1610
kikeestradadev <125921116+kikeestradadev@users.noreply.github.com>
2026-07-21 17:05:56 -0600

    limpiar repo

--- src/js/modules/utils.js
+++
@@ -1,8 +0,0 @@
-const initUtils = () => {
-	const yearEl = document.getElementById('footer-year');
-	if (yearEl) {
-		yearEl.textContent = new Date().getFullYear();
-	}
-};
-
-export default initUtils;
